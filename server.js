ab944876eae4b221852d9f9dff035ac9ae983d27
Kyle Hennig <henn.kyle@outlook.com>
2019-03-02 08:37:48 -0700

    Wrap server in a class and export

--- server.js
+++ server.js
@@ -2,87 +2,91 @@ import { axios } from 'axios';
 
 const url = 'http://cec2019.ca/'
 
-/**
- * Creates a new instance.
- */
-function createInstance() {
-    axios.post(url + '/instance')
-        .then(onResponse)
-        .catch(onError);
-}
-
-/**
- * Disconnects from the current instance.
- */
-function finish() {
-    axios.post(url + '/finish')
-        .then(onResponse)
-        .catch(onError);
-}
-
-/**
- * Turns the robot to face the specified direction.
- * @param {string} direction 
- */
-function turn(direction) {
-    if (!['N', 'E', 'S', 'W'].includes(direction)) {
-        console.log('Error: invalid direction.');
-        return;
+class Server {
+    /**
+     * Creates a new instance.
+     */
+    createInstance() {
+        axios.post(url + '/instance')
+            .then(_onResponse)
+            .catch(_onError);
     }
-    axios.post(url + `/finish/${direction}`)
-        .then(onResponse)
-        .catch(onError);
-}
 
-/**
- * Moves one block forward.
- */
-function move() {
-    axios.post(url + '/move')
-        .then(onResponse)
-        .catch(onError);
-}
+    /**
+     * Disconnects from the current instance.
+     */
+    finish() {
+        axios.post(url + '/finish')
+            .then(_onResponse)
+            .catch(_onError);
+    }
 
-/**
- * Scans for nearby trash.
- */
-function scanArea() {
-    axios.post(url + '/scanArea')
-        .then(onResponse)
-        .catch(onError);
-}
+    /**
+     * Turns the robot to face the specified direction.
+     * @param {string} direction 
+     */
+    turn(direction) {
+        if (!['N', 'E', 'S', 'W'].includes(direction)) {
+            console.log('Error: invalid direction.');
+            return;
+        }
+        axios.post(url + `/finish/${direction}`)
+            .then(_onResponse)
+            .catch(_onError);
+    }
 
-/**
- * Collects a previously scanned item.
- * @param {number} id The item's id.
- */
-function collectItem(id) {
-    axios.post(url + `/collectItem/${id}`)
-        .then(onResponse)
-        .catch(onError);
-}
+    /**
+     * Moves one block forward.
+     */
+    move() {
+        axios.post(url + '/move')
+            .then(_onResponse)
+            .catch(_onError);
+    }
 
-/**
- * Unloads an item.
- * Requires the robot is at the proper disposal bin.
- * @param {number} id The item's id.
- */
-function unloadItem(id) {
-    axios.post(url + `/unloadItem/${id}`)
-        .then(onResponse)
-        .catch(onError);
-}
+    /**
+     * Scans for nearby trash.
+     */
+    scanArea() {
+        axios.post(url + '/scanArea')
+            .then(_onResponse)
+            .catch(_onError);
+    }
 
-function onResponse(response) {
-    if (response.data.type === 'ERROR') {
+    /**
+     * Collects a previously scanned item.
+     * @param {number} id The item's id.
+     */
+    collectItem(id) {
+        axios.post(url + `/collectItem/${id}`)
+            .then(_onResponse)
+            .catch(_onError);
+    }
 
-    } else if (response.data.type === 'FAILURE') {
+    /**
+     * Unloads an item.
+     * Requires the robot is at the proper disposal bin.
+     * @param {number} id The item's id.
+     */
+    unloadItem(id) {
+        axios.post(url + `/unloadItem/${id}`)
+            .then(_onResponse)
+            .catch(_onError);
+    }
 
-    } else if (response.data.type === 'SUCCESS') {
+    _onResponse(response) {
+        if (response.data.type === 'ERROR') {
 
+        } else if (response.data.type === 'FAILURE') {
+
+        } else if (response.data.type === 'SUCCESS') {
+
+        }
+    }
+
+    _onError(error) {
+        console.log(error);
     }
 }
 
-function onError(error) {
-    console.log(error);
-}
+exports.Server = Server;
