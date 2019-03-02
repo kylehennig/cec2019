e5184391808b60daca2065ceb6815cc59c4d7db7
Mitchell <mbadam@ualberta.ca>
2019-03-02 11:34:56 -0500

    Fix main.js bugs

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-import (Server) from 'server';
+import Server from './server';
 
 
 class Board{
 
-    constructor(constants)){
-        let this.constants = constants;
+    constructor(constants){
+        this.constants = constants;
 
-        let binOrganicContents =0;
-        let binRecycleContents = 0;
-        let binTrashContents =0;
+        this.binOrganicContents =0;
+        this.binRecycleContents = 0;
+        this.binTrashContents =0;
         board.createSections();
     }
 
     createSections(){
         let rectangles = [];
         for(let i =this.Y_MIN+2; i < this.Y_MAX+3; i += 3) {
-            for (let j = this.X_MIN+this.SCAN_RADIUS+1; j < this.X_MAX+2*this.SCAN_RADIUS+1){
+            for (let j = this.X_MIN+this.SCAN_RADIUS+1; j < this.X_MAX+2*this.SCAN_RADIUS+1;j+= 2*this.SCAN_RADIUS +1){
                 let center = [];
                 if (i > this.Y_MAX) {
                     center[0] = this.Y_MAX;
@@ -27,23 +27,29 @@ class Board{
                                  done: false});
             }
         }
-        let this.rectangles = rectangles;
+        this.rectangles = rectangles;
     }
 }
 
 
-function main(){
+async function main(){
+
+    const server = new Server();
     // Connect
-    await response = Server.init();
-    let board;
+    try{
+        let response = await server.init();
+        let board;
 
-    if (response.type=== "FAILURE"){
-        console.log("Failed Init");
-    }else {
-        console.log("Init Success")
+        if (response.Response.type === "FAILURE"){
+            console.log("Failed Init");
+        }else {
+            console.log("Init Success")
 
-        // Board Setup
-        board = class Board(response.constants);
+            // Board Setup
+            board = new Board(response.constants);
+        }
+    } catch (err){
+        console.log(err);
     }
 
 
